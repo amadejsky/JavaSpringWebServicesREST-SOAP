92dfa852cf064aa3306a9b108d771d0239b6464e
amadejsky <adrian.madejski@vp.pl>
2024-08-01 17:28:37 +0200

    simple spring boot api created

---
+++ spring-boot-examples/src/main/java/com/amadejsky/spring_boot_examples/Controller.java
@@ -0,0 +1,19 @@
+package com.amadejsky.spring_boot_examples;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@RestController
+public class Controller {
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses(){
+        return Arrays.asList(
+                new Course(1,"Learn Docker","dockerCaptain"),
+                new Course(2,"Learn AWS","in28")
+        );
+    }
+}
