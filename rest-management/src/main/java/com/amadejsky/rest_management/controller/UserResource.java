45c14dceab157c8e661caf06fb5d431fae3f2321
amadejsky <adrian.madejski@vp.pl>
2024-08-12 09:12:16 +0200

    Internalization in spring configured and tested in postman, new internalization-test endpoint in controller

--- rest-management/src/main/java/com/amadejsky/rest_management/controller/UserResource.java
+++ rest-management/src/main/java/com/amadejsky/rest_management/controller/UserResource.java
@@ -4,25 +4,36 @@ import com.amadejsky.rest_management.exceptions.UserNotFoundException;
 import com.amadejsky.rest_management.models.User;
 import com.amadejsky.rest_management.service.UserDaoService;
 import jakarta.validation.Valid;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
+
 @RestController
 public class UserResource {
-    private UserDaoService userDaoService;
+    private MessageSource messageSource;
 
-    public UserResource(UserDaoService userDaoService) {
+    public UserResource(MessageSource messageSource, UserDaoService userDaoService) {
+        this.messageSource = messageSource;
         this.userDaoService = userDaoService;
     }
-    @GetMapping(path = "/users")
+
+    private UserDaoService userDaoService;
+
+//    public UserResource(UserDaoService userDaoService) {
+//        this.userDaoService = userDaoService;
+//    }
+    @GetMapping(path = "/users", produces = "application/json")
     public List<User> retrieveAllUsers(){
         return userDaoService.findAll();
     }
 
-    @GetMapping(path = "/users/{id}")
+    @GetMapping(path = "/users/{id}", produces = "application/json")
     public User retrieveUserById(@PathVariable int id){
         User user = userDaoService.findById(id);
         if(user==null)
@@ -30,7 +41,7 @@ public class UserResource {
         return user;
     }
 
-    @PostMapping(path = "/users")
+    @PostMapping(path = "/users", produces = "application/json")
     public ResponseEntity<User> addUser(@Valid @RequestBody User user){
         User savedUser = userDaoService.save(user);
         int userId = user.getId();
@@ -49,6 +60,12 @@ public class UserResource {
             throw new UserNotFoundException("id: "+id);
         userDaoService.deleteUser(id);
         return userDaoService.findAll();
+    }
+
+        @GetMapping(path="/test-i18")
+    public String helloWorldInternationalized(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",null,"Default message",locale);
     }
 
 }
