e8f8f44e1d0d3bc0d6d6b0f935f456032f09a66b
amadejsky <adrian.madejski@vp.pl>
2024-08-14 09:39:57 +0200

    Parametrized ? jdbc queries implemented

--- jpa/src/main/java/com/amadejsky/jpa/course/jdbc/JdbcRepository.java
+++ jpa/src/main/java/com/amadejsky/jpa/course/jdbc/JdbcRepository.java
@@ -1,6 +1,8 @@
 package com.amadejsky.jpa.course.jdbc;
 
+import com.amadejsky.jpa.models.Course;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -9,12 +11,33 @@ public class JdbcRepository {
     @Autowired
     private JdbcTemplate jdbc;
     private static String INSERT_QUERY =
-                """
-                        insert into course(id, name, author)
-                        values(1,'Angular','Muller');
-                """;
-    public void insert(){
-        jdbc.update(INSERT_QUERY);
+            """
+                    insert into course(id, name, author)
+                    values(?,?,?);
+            """;
+    private static String DELETE_QUERY =
+            """
+                    delete from course
+                    where id = ?;
+            """;
+    private static String SELECT_QUERY =
+            """
+                    select * from course
+                    where id = ?;
+            """;
+    public void insert(Course course){
+        jdbc.update(INSERT_QUERY,course.getId(),course.getName(),course.getAuthor());
     };
+    public void delete(long id){
+        jdbc.update(DELETE_QUERY,id);
+    }
+
+//    public void selectbyId(long id){
+//        jdbc.update(SELECT_QUERY,id);
+//    }
+    public Course selectByIdMappingObject(long id){
+        return jdbc.queryForObject(SELECT_QUERY,
+                new BeanPropertyRowMapper<>(Course.class),id);
+    }
 
 }
--- jpa/src/main/java/com/amadejsky/jpa/course/jdbc/jdbcCommandLineRunner.java
+++ jpa/src/main/java/com/amadejsky/jpa/course/jdbc/jdbcCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.amadejsky.jpa.course.jdbc;
 
+import com.amadejsky.jpa.models.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,11 @@ public class jdbcCommandLineRunner implements CommandLineRunner {
     private JdbcRepository jdbcRepository;
     @Override
     public void run(String... args) throws Exception {
-        jdbcRepository.insert();
+        jdbcRepository.insert(new Course(1,"Angular2024","Muller"));
+        jdbcRepository.insert(new Course(2,"LinuxAdmCommandL","H.Landa"));
+        jdbcRepository.delete(2);
+//        System.out.println(jdbcRepository.selectByIdMappingObject(1));
+//        System.out.println(jdbcRepository.selectByIdMappingObject(2));
+//        System.out.println(jdbcRepository.selectByIdMappingObject());
     }
 }
